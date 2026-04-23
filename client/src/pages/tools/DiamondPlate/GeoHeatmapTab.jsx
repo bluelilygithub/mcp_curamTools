c5d59cbdf0b35f3d34a5edfada9bbec11817e807
mrb <michaelbarrett@bluelily.com.au>
2026-04-24 09:11:15 +1000

    fix(geo-heatmap): switch to CartoDB tiles (OSM rate-limits prod domain)

--- client/src/pages/tools/DiamondPlate/GeoHeatmapTab.jsx
+++ client/src/pages/tools/DiamondPlate/GeoHeatmapTab.jsx
@@ -201,8 +201,10 @@ export default function GeoHeatmapTab({ startDate, endDate }) {
               scrollWheelZoom
             >
               <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                subdomains="abcd"
+                maxZoom={19}
               />
               {activeDataset.map((loc, i) => {
                 const count  = dataset === 'notInterested' ? loc.notInterested : loc.active;
